9513a21b3930dc0fc27afa0c74816b713314ac63
sherautcarsh <58589487+sherautcarsh@users.noreply.github.com>
2020-05-20 15:46:59 +0530

    Update issue.c

--- issue.c
+++ issue.c
@@ -13,9 +13,6 @@ int main() {
 	for(int i=0; i<t ;i++)
 	{
 		scanf("%d %d", &m[i], &n[i]);
-	}
-	for(int i=0; i<t ;i++)
-	{
 		if(m[i]>=1 && n[i]>=m[i] && n[i] <= 1000000000 && n[i]-m[i]<=100000)
 		{
 	    	for(int k = m[i]; k<= n[i];  k++)
@@ -28,18 +25,21 @@ int main() {
         	printf("\n");
 		}
 	}
-	return 0;
 }
 
 int prime(int a)
 {
     int b;
 	int count=0;
-	for (b=2;b<a;b++)
+	for (b=2;b< a;b++)
 	{
 		if(a%b!=0)
 		{
 			count++;
+		}
+		else
+		{
+			return 0;
 		}
 	}
 	return count;
